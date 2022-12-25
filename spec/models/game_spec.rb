54141ddad798e2799d2f710fc4d005a9c304ddc7
Andrew Haust <andrew@hau.st>
2022-12-25 18:56:17 -0500

    Add a describe block to spec

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -11,12 +11,14 @@ RSpec.describe Game, type: :model do
     ])
   end
 
-  it "errors when a third player tries to join a game" do
-    game = create(:game, :in_progress)
-    new_session = create(:session)
+  describe "#join" do
+    it "errors when a third player tries to join a game" do
+      game = create(:game, :in_progress)
+      new_session = create(:session)
 
-    game.join(new_session)
+      game.join(new_session)
 
-    expect(game.errors.full_messages.first).to eq("Cannot join game")
+      expect(game.errors.full_messages.first).to eq("Cannot join game")
+    end
   end
 end
