ac86e06ca3ef56ad8e23493d4118588e291043a6
Andrew Haust <andrew@hau.st>
2022-12-25 18:42:42 -0500

    Wrap starting a game in context

--- spec/system/game_spec.rb
+++ spec/system/game_spec.rb
@@ -11,44 +11,46 @@ RSpec.describe "Game", type: :system do
     expect(page).to have_content(/Create a game/)
   end
 
-  scenario "a user starts a game" do
-    create_a_game
+  feature "starting a game" do
+    scenario "a user starts a game" do
+      create_a_game
 
-    expect(page).to have_content(/Player 1: \d+/)
-  end
+      expect(page).to have_content(/Player 1: \d+/)
+    end
 
-  scenario "a user is redirected gack to the game they are playing" do
-    create_a_game
+    scenario "a user is redirected gack to the game they are playing" do
+      create_a_game
 
-    visit root_path
+      visit root_path
 
-    game = Game.first
-    expect(current_path).to eq(game_path(game))
-  end
+      game = Game.first
+      expect(current_path).to eq(game_path(game))
+    end
 
-  scenario "another user may join a game" do
-    game = create(:game)
+    scenario "another user may join a game" do
+      game = create(:game)
 
-    visit root_path
-    click_on "Join game #{game.id}"
+      visit root_path
+      click_on "Join game #{game.id}"
 
-    game.reload
+      game.reload
 
-    expect(current_path).to eq(game_path(game))
-    expect(page).to have_content(/Player 1: #{game.player_1.id}/)
-    expect(page).to have_content(/Player 2: #{game.player_2.id}/)
-  end
+      expect(current_path).to eq(game_path(game))
+      expect(page).to have_content(/Player 1: #{game.player_1.id}/)
+      expect(page).to have_content(/Player 2: #{game.player_2.id}/)
+    end
 
-  scenario "a third user tries to join" do
-    game = create(:game)
+    scenario "a third user tries to join" do
+      game = create(:game)
 
-    visit root_path
+      visit root_path
 
-    game.join(create(:session))
+      game.join(create(:session))
 
-    click_on "Join game #{game.id}"
+      click_on "Join game #{game.id}"
 
-    expect(page).to have_content(/Cannot join game/)
+      expect(page).to have_content(/Cannot join game/)
+    end
   end
 
   def create_a_game
