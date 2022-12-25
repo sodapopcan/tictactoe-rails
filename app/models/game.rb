990abfa73a847295419e1b34014120e3ed73158f
Andrew Haust <andrew@hau.st>
2022-12-25 18:59:34 -0500

    A player can make a move

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,10 @@
 class Game < ApplicationRecord
   BOARD = (0..8).map { nil }.freeze
+  WIN_CONDITIONS = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [6, 4, 2], [0, 4, 8]
+  ].freeze
 
   belongs_to :player_1, class_name: "Session"
   belongs_to :player_2, class_name: "Session", required: false
@@ -10,5 +15,10 @@ class Game < ApplicationRecord
     errors.add(:base, "Cannot join game") and return if player_2
 
     self.player_2 = player_2
+  end
+
+  def move(player, board_index)
+    self.board[board_index] = player.id
+    save
   end
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -11,6 +11,22 @@ RSpec.describe Game, type: :model do
     ])
   end
 
+  describe "#move" do
+    let(:game) { create(:game, :in_progress) }
+    let(:player_1) { game.player_1 }
+    let(:player_2) { game.player_2 }
+
+    it "player 1 can make a move" do
+      game.move(player_1, 0)
+
+      expect(game.reload.board).to eq([
+        player_1.id, nil, nil,
+        nil, nil, nil,
+        nil, nil, nil
+      ])
+    end
+  end
+
   describe "#join" do
     it "errors when a third player tries to join a game" do
       game = create(:game, :in_progress)
