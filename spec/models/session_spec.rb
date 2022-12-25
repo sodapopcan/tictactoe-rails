0b1fd025571ab6a96f2c90c629ee1f2064938807
Andrew Haust <andrew@hau.st>
2022-12-25 18:44:20 -0500

    Add an expectation to Session#current_game

--- spec/models/session_spec.rb
+++ spec/models/session_spec.rb
@@ -3,10 +3,12 @@ require 'rails_helper'
 RSpec.describe Session, type: :model do
   describe "#current_game" do
     it "returns the current game" do
-      session = create(:session)
-      game = create(:game, player_1: session)
+      session_1 = create(:session)
+      session_2 = create(:session)
+      game = create(:game, player_1: session_1, player_2: session_2)
 
-      expect(session.reload.current_game).to eq(game)
+      expect(session_1.reload.current_game).to eq(game)
+      expect(session_2.reload.current_game).to eq(game)
     end
   end
 end
