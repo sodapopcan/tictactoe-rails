def277d0a8200ec74c5ef03993080d6764a6d590
Andrew Haust <andrew@hau.st>
2022-12-25 15:08:07 -0500

    Ditch HomeController and just use GamesController

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,7 @@
 class GamesController < ApplicationController
+  def index
+  end
+
   def show
     @game = Game.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#index"
+  root "games#index"
   resources :games
 end
